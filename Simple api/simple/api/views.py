2347d51768249fe1e65fdc830e9310a8d775bace
Nitesh Kumar Chauhan <110373197+Niteshkumarchauhan@users.noreply.github.com>
2022-11-07 12:57:27 +0530

    Add files via upload

---
+++ Simple api/simple/api/views.py
@@ -0,0 +1,37 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from api.serializers import UserLoginSerializer,UserProfileSerializer, UserRegistrationSerializer
+from django.contrib.auth import authenticate
+from api.renderers import UserRenderer
+
+class UserRegistrationView(APIView):
+  renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = UserRegistrationSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+    return Response({ 'msg':'Registration Successful'}, status=status.HTTP_201_CREATED)
+
+class UserLoginView(APIView):
+  renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = UserLoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    email = serializer.data.get('email')
+    password = serializer.data.get('password')
+    user = authenticate(email=email, password=password)
+    if user is not None:
+      return Response({ 'msg':'Login Success'}, status=status.HTTP_200_OK)
+    else:
+      return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+
+class UserProfileView(APIView):
+  renderer_classes = [UserRenderer]
+  def get(self, request, format=None):
+    serializer = UserProfileSerializer(request.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
